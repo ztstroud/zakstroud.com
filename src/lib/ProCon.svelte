f09908b0a1f8a1434c1726aa10b93f8dc1a84f9e
Zachary Stroud <ztstroud@gmail.com>
2025-01-26 12:09:48 -0800

    wip: add ProCon component

---
+++ src/lib/ProCon.svelte
@@ -0,0 +1,33 @@
+<div class="cols">
+    <div>
+        <div class="title">
+            <b>Pros</b>
+        </div>
+        <slot name="pros" />
+    </div>
+    <div class="divider"></div>
+    <div>
+        <div class="title">
+            <b>Cons</b>
+        </div>
+        <slot name="cons" />
+    </div>
+</div>
+
+<style>
+    .cols {
+        display: grid;
+
+        grid-template-columns: 1fr 1px 1fr;
+        gap: 1em;
+    }
+
+    .title {
+        text-align: center;
+    }
+
+    .divider {
+        background-color: currentcolor;
+    }
+</style>
+
